a4378980acc64043693e28c3ebe03a8af9071073
Govind Dandekar <govind.dandekar@gmail.com>
2025-08-10 16:28:52 -0700

    add ThemeProvider for dark/light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+
 import "./globals.css";
+
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -19,11 +22,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={cn(poppins.className, "dark")}
+        className={poppins.className}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ ThemeProvider>
       </body>
     </html>
   );
